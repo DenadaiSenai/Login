caa1e7f03dd9c98b0e154480ce8f86f60dc8e932
DenadaiSenai <marcio.denadai@gmail.com>
2023-10-17 16:17:33 -0300

    Modularizado com EJS

--- app.js
+++ app.js
@@ -36,37 +36,74 @@ app.set('view engine', 'ejs');
 
 // Rota para a página de login
 app.get('/', (req, res) => {
-    res.render('login');
+    res.render('pages/index');
+});
+
+// Página de login
+app.get('/login', (req, res) => {
+    res.render('pages/login');
+});
+
+app.get('/about', (req, res) => {
+    res.render('pages/about')
 });
 
 // Rota para processar o formulário de login
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
 
-    const query = 'SELECT * FROM users WHERE username = ? AND password = ?';
+    const query = 'SELECT * FROM users WHERE username = ? AND password = SHA1(?)';
 
     db.query(query, [username, password], (err, results) => {
         if (err) throw err;
-
+        
         if (results.length > 0) {
             req.session.loggedin = true;
             req.session.username = username;
             res.redirect('/dashboard');
         } else {
-            res.send('Credenciais incorretas. <a href="/">Tente novamente</a>');
+            // res.send('Credenciais incorretas. <a href="/">Tente novamente</a>');
+            res.redirect('/login_failed');
         }
     });
+});
+
+// Rotas para cadastrar
+app.get('/cadastrar', (req, res) => {
+    res.render('pages/cadastrar');
+});
+
+app.post('/cadastrar', (req, res) => {
+    const { username, password } = req.body;
+
+    const query = 'INSERT INTO users (username, password) VALUES (?, SHA1(?))';
+
+    db.query(query, [username, password], (err, results) => {
+        if (err) throw err;
+        
+        if (results.length > 0) {
+            req.session.loggedin = true;
+            req.session.username = username;
+            res.redirect('/dashboard');
+        } else {
+            // res.send('Credenciais incorretas. <a href="/">Tente novamente</a>');
+            res.redirect('/login_failed');
+        }
+    });
+});
+
+app.get('/login_failed', (req, res) => {
+    res.render('pages/login_failed');
 });
 
 // Rota para a página do painel
 app.get('/dashboard', (req, res) => {
     //
-    //
-    //
     //modificação aqui
     if (req.session.loggedin) {
         //res.send(`Bem-vindo, ${req.session.username}!<br><a href="/logout">Sair</a>`);
-        res.sendFile(__dirname + '/index.html');
+        // res.sendFile(__dirname + '/index.html');
+        res.render('pages/dashboard');
     } else {
         res.send('Faça login para acessar esta página. <a href="/">Login</a>');
     }
